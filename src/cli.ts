1e0134226b8016abad7507ba2ec2ca4f16beb8bc
droppedcode <herendibalazs@gmail.com>
2024-04-28 08:55:16 +0200

    Add config argument to cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
+import fs = require('fs');
 import os = require('os');
+import path = require('path');
 
 import * as commandLineArgs from 'command-line-args';
 
@@ -9,6 +11,7 @@ import { CreateMode, HeaderMode, Options } from './options';
 
 const args = commandLineArgs(
   [
+    { name: 'config', alias: 'c', type: String },
     { name: 'path', alias: 'p', type: String, multiple: true },
     { name: 'out', alias: 'o', type: String },
 
@@ -65,27 +68,52 @@ const args = commandLineArgs(
   }
 );
 
-const options: Partial<Options> = {
+let options: Partial<Options> = {};
+
+if (args.config) {
+  const configPath = path.isAbsolute(args.config)
+    ? args.config
+    : path.resolve(args.config);
+  if (!fs.existsSync(configPath)) {
+    console.log(`Configuration file '${configPath}' is not found.`);
+  } else {
+    options = <Partial<Options>>(
+      JSON.parse(fs.readFileSync(configPath).toString())
+    );
+  }
+}
+
+options = {
   paths:
     args.path ??
+    options.paths ??
     (args._unknown && args._unknown.length > 0
       ? args._unknown.slice(0, args._unknown.length - 1)
       : undefined),
   output:
     args.out ??
+    options.output ??
     (args._unknown && args._unknown.length > 1
       ? args._unknown[args._unknown.length - 1]
       : undefined),
-  mode: args.mode,
-  includes: args.include?.map((m: string) => new RegExp(m)),
-  excludes: args.exclude?.map((m: string) => new RegExp(m)),
-  newline: args.eol,
-  newlineAtTheEndOfFile: args.eolAtEof,
-  header: args.header,
-  headerMode: args.headerMode,
-  createFileOnlyIfNeeded: args.ifNeeded,
-  format: args.format,
+  mode: args.mode ?? options.mode,
+  includes:
+    args.include?.map((m: string) => new RegExp(m)) ??
+    // It is string, we convert it to regex
+    options.includes?.map((m: RegExp) => new RegExp(m)),
+  excludes:
+    args.exclude?.map((m: string) => new RegExp(m)) ??
+    // It is string, we convert it to regex
+    options.excludes?.map((m: RegExp) => new RegExp(m)),
+  newline: args.eol ?? options.newline,
+  newlineAtTheEndOfFile: args.eolAtEof ?? options.newlineAtTheEndOfFile,
+  header: args.header ?? options.header,
+  headerMode: args.headerMode ?? options.headerMode,
+  createFileOnlyIfNeeded: args.ifNeeded ?? options.createFileOnlyIfNeeded,
+  format: args.format ?? options.format,
 };
+
+console.log(options);
 
 const generator = new IndexGenerator(options);
 generator.generate();
